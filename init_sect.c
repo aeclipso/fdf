ce605aae86069e179fbe90efc785fbc7a56fc181
aeclipso <aeclipso@student.21-school.ru>
2020-11-10 17:37:52 +0300

    in_s

--- init_sect.c
+++ init_sect.c
@@ -6,7 +6,7 @@
 /*   By: aeclipso <aeclipso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/09 16:56:56 by aeclipso          #+#    #+#             */
-/*   Updated: 2020/11/09 17:02:31 by aeclipso         ###   ########.fr       */
+/*   Updated: 2020/11/10 17:35:18 by aeclipso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,10 @@ float		init_sect(t_fdf *fdf)
 	float	sect_h;
 	float	sect_w;
 
-	sect_h = 864 / (fdf->map->height * 2);
-	sect_w = 1536 / (fdf->map->width * 2);
+	sect_h = (fdf->image.h - (fdf->image.h / 5)) / (fdf->map->height * 2);
+	sect_w = (fdf->image.w - (fdf->image.w / 5)) / (fdf->map->width * 2);
 
+	ft_printf("SECT_H = %3d\nSECT_W = %3d\n", sect_h, sect_w);
 	if ((int)sect_h < (int)sect_w)
 		return (sect_h);
 	else
